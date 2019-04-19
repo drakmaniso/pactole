d6c2b3e8f7a4228a3eeb4aed1a1dcb3befe24b36
Laurent Moussault <moussault.laurent@gmail.com>
2019-04-19 12:06:25 +0200

    Improve testing

--- main/accounting_test.js
+++ main/accounting_test.js
@@ -1,45 +1,54 @@
 'use strict'
 
+const assert = require('assert').strict
 const acc = require('./accounting.js')
 
 let l = new acc.Ledger('simple')
-console.log(l)
-console.log(l.setDescription('My simple ledger'))
+assert.ok(l instanceof acc.Ledger)
+assert.equal(l.name, 'simple')
+l.setDescription('My simple ledger')
+assert.equal(l.description, 'My simple ledger')
 
 let foo = new acc.Account('foo')
-console.log(foo)
+assert.ok(foo instanceof acc.Account)
+assert.equal(foo.name, 'foo')
 
 let bar = new acc.Account('bar')
-let foobar = new acc.Account('foo')
-let baz = new acc.Account('baz', bar)
+assert.ok(bar instanceof acc.Account)
 
-console.log(l.addAccount(foo))
-console.log(l.addAccount(baz))
-console.log(l.addAccount(bar))
-console.log(l.addAccount(foobar))
-console.log(l.addAccount(baz))
+let foobar = new acc.Account('foo')
+assert.ok(foobar instanceof acc.Account)
+
+let baz = new acc.Account('baz', bar)
+assert.ok(baz instanceof acc.Account)
+
+assert.ok(l.addAccount(foo) instanceof acc.Ledger)
+assert.ok(l.addAccount(baz) instanceof Error)
+assert.ok(l.addAccount(bar) instanceof acc.Ledger)
+assert.ok(l.addAccount(foobar) instanceof Error)
+assert.ok(l.addAccount(baz) instanceof acc.Ledger)
 
 let e = new acc.Transaction(new Date(), [], [])
-console.log(e)
+assert.ok(e instanceof Error, e)
 e = new acc.Transaction(new Date(), [new acc.Debit(foo, 33)], [])
-console.log(e)
+assert.ok(e instanceof Error)
 e = new acc.Transaction(new Date(), [new acc.Debit(foo, 22)], [new acc.Credit(bar, 33)])
-console.log(e)
+assert.ok(e instanceof Error)
 e = new acc.Transaction(new Date(), [new acc.Credit(foo, 33)], [new acc.Credit(bar, 33)])
-console.log(e)
+assert.ok(e instanceof Error)
 e = new acc.Transaction(new Date(), [new acc.Debit(foo, 33)], [new acc.Debit(bar, 33)])
-console.log(e)
+assert.ok(e instanceof Error)
 
 let t1 = new acc.Transaction(
   new Date(),
   [new acc.Debit(foo, 33)],
   [new acc.Credit(bar, 33)]
 )
-console.log(t1)
+assert.ok(t1 instanceof acc.Transaction)
 
 let t2 = new acc.Transaction(
   new Date(),
   [new acc.Debit(foo, 20), new acc.Debit(foo, 80)],
   [new acc.Credit(bar, 70), new acc.Credit(baz, 30)]
 )
-console.log(t2)
+assert.ok(t2 instanceof acc.Transaction)
