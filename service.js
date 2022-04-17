0735af4c12366c3facbfae9febfa7e508f6c71f3
drakmaniso <moussault.laurent@gmail.com>
2022-04-17 08:34:54 +0200

    Improve dialog animation

--- service.js
+++ service.js
@@ -23,7 +23,7 @@ const version = 2
 const staticCacheName = "pactole-cache-2"
 
 // Used to force an update on client-side
-const serviceVersion = "1.4.0b32"
+const serviceVersion = "1.4.0b33"
 
 
 self.addEventListener('install', event => {
