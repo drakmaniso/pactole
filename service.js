9725f6a969b5bb0fcca4d8d5c42f4aa67131b3e6
drakmaniso <moussault.laurent@gmail.com>
2022-05-04 11:41:26 +0200

    Bump service version before release

--- service.js
+++ service.js
@@ -23,7 +23,7 @@ const version = 2
 const staticCacheName = "pactole-cache-2"
 
 // Used to force an update on client-side
-const serviceVersion = "1.4.4"
+const serviceVersion = "1.4.5"
 
 
 self.addEventListener('install', event => {
