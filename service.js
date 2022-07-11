3b1886982da2514134bca036a25e4d2311708242
drakmaniso <moussault.laurent@gmail.com>
2022-07-11 09:58:38 +0200

    Prepare for release of 1.5.6

--- service.js
+++ service.js
@@ -24,7 +24,7 @@ const version = 2
 const staticCacheName = "pactole-cache-2"
 
 // Used to force an update on client-side
-const serviceVersion = "1.5.5beta"
+const serviceVersion = "1.5.6"
 
 
 self.addEventListener('install', event => {
